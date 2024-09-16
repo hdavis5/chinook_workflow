226ea8cfb6df855c0ce92f5cffdad6b633d4507e
Hayden <hrdavis1@uw.edu>
2024-09-16 16:51:00 -0700

    new scripts folder and moved filtering.sh

---
+++ scripts/filtering.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Bash script to filter input bcf file and output both a vcf and the plink format (BED/BIM/FAM) files
+
+INPUT_BCF="input.bcf"
+OUTPUT_PREFIX="CC_filtered"
+
+#Convert BCF to both VCF and PLINK format
+plink --bcf $INPUT_BCF --make-bed --recode vcf --out ${OUTPUT_PREFIX}_step1
+
+#Filter variants with >5% missingness
+plink --bfile ${OUTPUT_PREFIX}_step1 --geno 0.05 --make-bed --recode vcf --out ${OUTPUT_PREFIX}_step2
+
+#Filter individuals with >10% missingness
+plink --bfile ${OUTPUT_PREFIX}_step2 --mind 0.1 --make-bed --recode vcf --out ${OUTPUT_PREFIX}_step3
+
+#Filter MAF < 5%
+plink --bfile ${OUTPUT_PREFIX}_step3 --maf 0.05 --make-bed --recode vcf --out ${OUTPUT_PREFIX}_step4
+
+#LD pruning
+plink --bfile ${OUTPUT_PREFIX}_step4 --indep-pairwise 100 10 0.2 --out ${OUTPUT_PREFIX}_step5
+plink --bfile ${OUTPUT_PREFIX}_step4 --extract ${OUTPUT_PREFIX}_step5.prune.in --make-bed --recode vcf --out ${OUTPUT_PREFIX}_step6
+
+#Remove Hardy-Weinberg equilibrium outliers
+plink --bfile ${OUTPUT_PREFIX}_step6 --hwe 1e-6 --make-bed --recode vcf --out ${OUTPUT_PREFIX}_final
