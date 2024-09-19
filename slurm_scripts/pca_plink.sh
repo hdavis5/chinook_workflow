4df87266107dd3212bc47706b4559e12b9a8fe5d
Hayden <hrdavis1@uw.edu>
2024-09-19 11:53:44 -0700

    PCA script and slurm file

---
+++ slurm_scripts/pca_plink.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=plink_pca_analysis
+#SBATCH --mail-user=hayden.davis@noaa.gov
+#SBATCH --mail-type=ALL
+#SBATCH -c 10
+#SBATCH --mem=8G
+#SBATCH --time=2:00:00
+#SBATCH --output=/path/to/output/pca_analysis.out
+#SBATCH --error=/path/to/output/pca_analysis.err
+
+# Load modules for PLINK and Python
+module load bio/plink/1.90b6.23
+
+# Variables
+BFILE="/home/hdavis/catherine_creek/bcf_files/filtered_vcf/CC_filtered_final"
+OUTFILE="/home/hdavis/catherine_creek/chinook_workflow/data/pca_results"
+
+# Step 1: Run PLINK PCA
+plink --bfile $BFILE --pca --out $OUTFILE
