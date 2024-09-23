2ff44edea77c66c2e65c49ed9f1a2a02cc1b46c9
Hayden <hrdavis1@uw.edu>
2024-09-23 16:42:20 -0700

    updated method

--- slurm_scripts/chr_plink.sh
+++ slurm_scripts/chr_plink.sh
@@ -14,9 +14,12 @@ module load bio/plink/1.90b6.23
 
 # Variables
 BFILE="/home/hdavis/catherine_creek/bcf_files/filtered_vcf/CC_filtered_final"
-OUTFILE="/home/hdavis/catherine_creek/chinook_workflow/data/pca_results"
+OUTFILE="/home/hdavis/catherine_creek/chinook_workflow/data/"
 OUTPUT_PREFIX="CC_chr_filtered"
 CHR_MAP="/home/hdavis/catherine_creek/chinook_workflow/data/chr_map.txt"
 
-# Step 1: Run PLINK PCA
-plink --bfile $BFILE --make-bed --recode vcf --allow-extra-chr --double-id --chr-set 34 --threads 10 --out $OUTFILE --update-chr $CHR_MAP/$OUTPUT_PREFIX
+# Step 1: Update chromosome names in PLINK format
+plink --bfile $BFILE --make-bed --allow-extra-chr --double-id --chr-set 34 --update-chr $CHR_MAP --out ${OUTPUT_PREFIX}
+
+# Step 2: Convert the updated PLINK dataset back to VCF format
+plink --bfile ${OUTPUT_PREFIX} --recode vcf --allow-extra-chr --double-id --chr-set 34 --out ${OUTPUT_PREFIX}
