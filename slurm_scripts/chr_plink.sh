d784a2f363dad7cfab4a07be7853fe59c04c1fba
Hayden <hrdavis1@uw.edu>
2024-09-24 10:49:01 -0700

    added remove duplicate step

--- slurm_scripts/chr_plink.sh
+++ slurm_scripts/chr_plink.sh
@@ -18,12 +18,22 @@ OUTFILE="/home/hdavis/catherine_creek/chinook_workflow/data/"
 OUTPUT_PREFIX="CC_chr_filtered"
 CHR_MAP="/home/hdavis/catherine_creek/chinook_workflow/data/chr_map.txt"
 
-plink --bfile $BFILE --make-bed --allow-extra-chr --double-id --chr-set 34 \
-      --update-chr $CHR_MAP --set-missing-var-ids @:#:$1:$2 \
-      --out ${OUTPUT_PREFIX}
 
-# Step 2: Convert the updated PLINK dataset back to VCF format
-plink --bfile ${OUTPUT_PREFIX} --recode vcf --allow-extra-chr --double-id --chr-set 34 --out $OUTFILE
+plink --bfile $BFILE --allow-extra-chr --chr-set 34 --rm-dup force-first --make-bed --out ${OUTPUT_PREFIX}_dedup
 
-# Step 3: Perform any further analysis, such as PCA, using the updated dataset
-plink --bfile ${OUTPUT_PREFIX} --pca --allow-extra-chr --double-id --chr-set 34 --threads 10 --out $OUTFILE
+# Step 2: Update chromosome names in the deduplicated dataset
+plink --bfile ${OUTPUT_PREFIX}_dedup --make-bed --allow-extra-chr --double-id --chr-set 34 \
+      --update-chr $CHR_MAP --out ${OUTPUT_PREFIX}_updated
+
+# Step 3: Convert the updated PLINK dataset back to VCF format
+plink --bfile ${OUTPUT_PREFIX}_updated --recode vcf --allow-extra-chr --double-id --chr-set 34 --out $OUTFILE
+#
+#plink --bfile $BFILE --make-bed --allow-extra-chr --double-id --chr-set 34 \
+#      --update-chr $CHR_MAP --set-missing-var-ids @:#:$1:$2 \
+#      --out ${OUTPUT_PREFIX}
+#
+## Step 2: Convert the updated PLINK dataset back to VCF format
+#plink --bfile ${OUTPUT_PREFIX} --recode vcf --allow-extra-chr --double-id --chr-set 34 --out $OUTFILE
+#
+## Step 3: Perform any further analysis, such as PCA, using the updated dataset
+#plink --bfile ${OUTPUT_PREFIX} --pca --allow-extra-chr --double-id --chr-set 34 --threads 10 --out $OUTFILE
