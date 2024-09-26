8d8fd701ae0915bc5aa7502b1378d8d9dede8a40
Hayden <hrdavis1@uw.edu>
2024-09-26 11:54:13 -0700

    going back to early version

--- scripts/filtering.sh
+++ scripts/filtering.sh
@@ -3,26 +3,26 @@
 #Bash script to filter input bcf file and output both a vcf and the plink format (BED/BIM/FAM) files
 
 INPUT_VCF="/home/hdavis/catherine_creek/bcf_files/combined2024-08-19.bcf"
-OUTPUT_PREFIX="/home/hdavis/catherine_creek/bcf_files/CC"
-CHR_MAP="/home/hdavis/catherine_creek/chinook_workflow/data/chr_map.txt"
+OUTPUT_PREFIX="CC_filtered"
 
-plink --vcf $INPUT_VCF --make-bed --recode vcf --allow-extra-chr --make-bed --double-id --threads 10 --out ${OUTPUT_PREFIX}_step_convert
+#Convert BCF to both VCF and PLINK format
+plink --vcf $INPUT_VCF --make-bed --recode vcf --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_step1
 
 #Filter variants with >5% missingness
-plink --bfile ${OUTPUT_PREFIX}_step_convert --geno 0.05 --allow-extra-chr --recode vcf --make-bed --double-id --threads 10 --out ${OUTPUT_PREFIX}_step1
+plink --bfile ${OUTPUT_PREFIX}_step1 --geno 0.05 --allow-extra-chr --make-bed --recode vcf --double-id --threads 10 --out ${OUTPUT_PREFIX}_step2
 
 #Filter individuals with >10% missingness
-plink --bfile ${OUTPUT_PREFIX}_step1 --mind 0.1 --allow-extra-chr --recode vcf --make-bed --double-id --threads 10 --out ${OUTPUT_PREFIX}_step2
+plink --bfile ${OUTPUT_PREFIX}_step2 --mind 0.1 --allow-extra-chr --make-bed --recode vcf --double-id --threads 10 --out ${OUTPUT_PREFIX}_step3
 
 #Filter MAF < 5%
-plink --bfile ${OUTPUT_PREFIX}_step2 --maf 0.05 --allow-extra-chr --recode vcf --make-bed --double-id --threads 10 --out ${OUTPUT_PREFIX}_step3
+plink --bfile ${OUTPUT_PREFIX}_step3 --maf 0.05 --allow-extra-chr --make-bed --recode vcf --double-id --threads 10 --out ${OUTPUT_PREFIX}_step4
 
 #LD pruning
-plink --bfile ${OUTPUT_PREFIX}_step3 --indep-pairwise 100 10 0.2 --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_step4
-plink --bfile ${OUTPUT_PREFIX}_step3 --extract ${OUTPUT_PREFIX}_step4.prune.in --allow-extra-chr --recode vcf --make-bed --threads 10 --out ${OUTPUT_PREFIX}_step5
+plink --bfile ${OUTPUT_PREFIX}_step4 --indep-pairwise 100 10 0.2 --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_step5
+plink --bfile ${OUTPUT_PREFIX}_step4 --extract ${OUTPUT_PREFIX}_step5.prune.in --allow-extra-chr --make-bed --recode vcf --out ${OUTPUT_PREFIX}_step6
 
 #Remove Hardy-Weinberg equilibrium outliers
-plink --bfile ${OUTPUT_PREFIX}_step5 --hwe 1e-6 --make-bed --recode vcf --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_filtered
+plink --bfile ${OUTPUT_PREFIX}_step6 --hwe 1e-6 --make-bed --recode vcf --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_final
 
 bcftools annotate --rename-chrs $CHR_MAP -o ${OUTPUT_PREFIX}_filterRename -O v ${OUTPUT_PREFIX}_filtered
 
