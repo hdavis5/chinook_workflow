9496ae321a52f45f877ce5b5a345d8238a995c5b
Hayden <hrdavis1@uw.edu>
2024-09-26 11:04:52 -0700

    updated process to only generate vcf file on the final step after filtering has been done

--- scripts/filtering.sh
+++ scripts/filtering.sh
@@ -6,24 +6,20 @@ INPUT_VCF="/home/hdavis/catherine_creek/bcf_files/combined2024-08-19.bcf"
 OUTPUT_PREFIX="/home/hdavis/catherine_creek/bcf_files/CC"
 CHR_MAP="/home/hdavis/catherine_creek/chinook_workflow/data/chr_map.txt"
 
-#Convert BCF to both VCF and PLINK format
-plink --vcf $INPUT_VCF --recode vcf --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}
-#plink --vcf $INPUT_VCF --make-bed --recode vcf --allow-extra-chr --double-id --chr-set 34 --threads 10 --out ${OUTPUT_PREFIX}_step1
+#Filter variants with >5% missingness
+plink --vcf $INPUT_VCF --geno 0.05 --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_temp_step1
 
-#plink --bfile ${OUTPUT_PREFIX}_step1 --allow-extra-chr --make-bed --double-id --chr-set 34 --threads 10 --update-chr $CHR_MAP --out ${OUTPUT_PREFIX}_chr
+#Filter individuals with >10% missingness
+plink --vcf ${OUTPUT_PREFIX}_temp_step1 --mind 0.1 --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_temp_step2
 
-##Filter variants with >5% missingness
-#plink --bfile ${OUTPUT_PREFIX}_step1 --geno 0.05 --allow-extra-chr --make-bed --recode vcf --double-id --chr-set 34 --threads 10 --out ${OUTPUT_PREFIX}_step2
-#
-##Filter individuals with >10% missingness
-#plink --bfile ${OUTPUT_PREFIX}_step2 --mind 0.1 --allow-extra-chr --make-bed --recode vcf --double-id --chr-set 34 --threads 10 --out ${OUTPUT_PREFIX}_step3
-#
-##Filter MAF < 5%
-#plink --bfile ${OUTPUT_PREFIX}_step3 --maf 0.05 --allow-extra-chr --make-bed --recode vcf --double-id --chr-set 34 --threads 10 --out ${OUTPUT_PREFIX}_step4
-#
-##LD pruning
-#plink --bfile ${OUTPUT_PREFIX}_step4 --indep-pairwise 100 10 0.2 --allow-extra-chr --double-id --chr-set 34 --threads 10 --out ${OUTPUT_PREFIX}_step5
-#plink --bfile ${OUTPUT_PREFIX}_step4 --extract ${OUTPUT_PREFIX}_step5.prune.in --allow-extra-chr --make-bed --chr-set 34 --threads 10 --recode vcf --out ${OUTPUT_PREFIX}_step6
-#
-##Remove Hardy-Weinberg equilibrium outliers
-#plink --bfile ${OUTPUT_PREFIX}_step6 --hwe 1e-6 --make-bed --recode vcf --allow-extra-chr --double-id --chr-set 34 --threads 10 --out ${OUTPUT_PREFIX}_final
+#Filter MAF < 5%
+plink --vcf ${OUTPUT_PREFIX}_temp_step2 --maf 0.05 --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_temp_step3
+
+#LD pruning
+plink --vcf ${OUTPUT_PREFIX}_temp_step3 --indep-pairwise 100 10 0.2 --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_temp_step4
+plink --vcf ${OUTPUT_PREFIX}_temp_step3 --extract ${OUTPUT_PREFIX}_temp_step4.prune.in --allow-extra-chr --threads 10 --out ${OUTPUT_PREFIX}_temp_step5
+
+#Remove Hardy-Weinberg equilibrium outliers
+plink --vcf ${OUTPUT_PREFIX}_temp_step5 --hwe 1e-6 --make-bed --recode vcf --allow-extra-chr --double-id --threads 10 --out ${OUTPUT_PREFIX}_filtered
+
+rm ${OUTPUT_PREFIX}_temp*
