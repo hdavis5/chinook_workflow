44e80e7cc515c121f6e92c65745ac65f411a7ca4
Hayden <hrdavis1@uw.edu>
2024-09-26 12:51:28 -0700

    trying to filter

--- slurm_scripts/bcf_convert.sh
+++ slurm_scripts/bcf_convert.sh
@@ -18,12 +18,17 @@ module load bio/bcftools/1.11
 
 # change reference, samples, and SAMPLES output directories as needed
 BCF=/home/hdavis/catherine_creek/bcf_files/filtered_vcf/CC_filtered_final.vcf
+GZ=/home/hdavis/catherine_creek/bcf_files/CC_filtered_final.vcf.gz
 OUT=/home/hdavis/catherine_creek/bcf_files/cc_renamed.vcf
 CHR_MAP=/home/hdavis/catherine_creek/bcf_files/filtered_vcf/chr_map.txt
 
+bgzip -c $BCF > $GZ
+
+bcftools index -t $GZ
+
 # Commands
 #update chromosome names
-bcftools annotate --rename-chrs $CHR_MAP -o $OUT -O v $BCF
+bcftools annotate --rename-chrs $CHR_MAP -o $OUT -O v $GZ
 
 # Remove contigs that are not associated with chromosomes
-bcftools view -r ^NW_ ${OUT} -o cc_filtered.vcf -O v ${OUT}_trimmed.vcf
+bcftools view -r ^NW_ $OUT -o cc_filtered.vcf -O v
